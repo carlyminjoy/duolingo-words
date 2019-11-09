83925fa3c49bf034a682584e6b490168f2be28ec
Carly Minjoy <cminjoy@myune.edu.au>
2019-11-09 22:49:49 +1000

    working app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="#78c800"
+      dark
+    >
+      <div class="d-flex align-center text-center">
+        <h1>My Duolingo Words</h1>
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'app',
+  name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang='scss' scoped>
+body {
+    font-family: din-round,sans-serif!important;
+
+    .v-toolbar__content {
+        max-width: 1200px;
+        margin: 0 auto;
+    }
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,148 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+	<v-container>
+		<v-tabs color='#78c800'>
+			<v-tab @click='pos = "Adjective"'><a>Adjective</a></v-tab>
+			<v-tab @click='pos = "Noun"'><a>Noun</a></v-tab>
+			<v-tab @click='pos = "Verb"'><a>Verb</a></v-tab>
+			<v-tab @click='pos = "Proper noun"'><a>Proper noun</a></v-tab>
+			<v-tab @click='pos = "Pronoun"'><a>Pronoun</a></v-tab>
+			<v-tab @click='pos = "Adverb"'><a>Adverb</a></v-tab>
+			<v-tab @click='pos = "Preposition"'><a>Preposition</a></v-tab>
+			<v-tab @click='pos = "Determiner"'><a>Determiner</a></v-tab>
+			<v-tab @click='pos = "Numeral"'><a>Numeral</a></v-tab>
+			<v-tab @click='pos = "Interjection"'><a>Interjection</a></v-tab>
+			<v-tab @click='pos = "Conjunction"'><a>Conjunction</a></v-tab>
+		</v-tabs>
+		<v-layout
+			text-center
+			wrap
+		>
+		<v-container fluid>
+			<v-row class='word-container' v-for='(word,index) in filteredWords' :key='index'>
+				<v-col class='word' cols='6'  
+					@click='search(index)'>
+					<h3>{{ word.normalized_string }} </h3>
+					<p>{{ word.pos }}</p>
+				</v-col>
+				<v-col v-if='word.translation' class='translation' cols='6'>
+					<h3>{{ word.translation }}</h3>
+				</v-col>
+			</v-row>
+		</v-container>
+
+		</v-layout>
+	</v-container>
 </template>
 
 <script>
+import axios from 'axios'
+import { default as words } from '../words'
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+	name: 'HelloWorld',
+
+	data: () => ({
+		pos: null,
+		words: words.vocab_overview.map(w => {
+			w.translation = null;
+			return w
+		}).sort((a, b) => a.strength - b.strength)
+	}),
+	computed: {
+		filteredWords() {
+			let vm = this;
+			if (!vm || !vm.words) return []
+			return vm.words.filter(w => !vm.pos || w.pos == vm.pos)
+		}
+	},
+	methods: {
+		search (index) {
+			let vm = this;
+
+			var data = `source=es&q=${encodeURI(vm.filteredWords[index].normalized_string)}&target=en`;
+
+			var xhr = new XMLHttpRequest();
+			xhr.withCredentials = true;
+
+			xhr.addEventListener("readystatechange", function () {
+				if (this.readyState === this.DONE) {
+					console.log(vm.filteredWords[index])
+					vm.filteredWords[index].translation = JSON.parse(this.responseText).data.translations[0].translatedText;
+					console.log(vm.filteredWords[index])
+				}
+			});
+
+			xhr.open("POST", "https://google-translate1.p.rapidapi.com/language/translate/v2");
+			xhr.setRequestHeader("x-rapidapi-host", "google-translate1.p.rapidapi.com");
+			xhr.setRequestHeader("x-rapidapi-key", "b82a516a4bmshd9e853bb88fc02bp1f25a5jsn6a067c1ea61d");
+			xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+
+			xhr.send(data);
+		}
+	},
+	mounted: () => {
+		let vm = this;
+	}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style lang='scss' scoped>
+.layout {
+	max-width: 600px;
+	margin: 0 auto;
+
+	.tab, a {
+		color : #78c800;
+	}
+	.word-container {
+		height: 60px;
+		margin: 10px;
+
+		.word {
+			cursor: pointer;
+			font-weight: 400;
+			transition: 0.3s ease;
+			display:flex;
+			justify-content:space-between;
+			align-items: center;
+			padding: 15px;
+
+			h3,p {
+				display:inline-block;
+				margin: 0;
+			}
+
+			p {
+				color: #78c800;
+			}
+		}
+
+		&:hover {
+			.word {
+				transform: scale(1.05);
+				color: #78c800;
+				font-weight: 800;
+			}
+
+			.translation {
+				display:block;
+			}
+		}
+	}
+
+	.translation {
+		display: none;
+		text-align:center;
+
+		h3 {
+			padding: 10px 20px;
+			color: #fff;
+			background: #78c800;
+			text-align:center;
+			border-radius: 15px;
+			border: 2px solid #fff;
+			display:inline-block;
+		}
+	}
 }
 </style>
